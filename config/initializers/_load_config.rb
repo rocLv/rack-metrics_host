ea53e6af0f9e06f9940314087273b67b68221d60
Greg Molnar <molnargerg@gmail.com>
2014-11-06 10:07:48 +0000

    add config

---
+++ config/initializers/_load_config.rb
@@ -0,0 +1,16 @@
+default_config_file = Rails.root.join("config", "config.example.yml")
+config_file = Rails.env == "test" ? default_config_file : Rails.root.join("config", "config.yml")
+# Load config if config file exists.
+# if File.exists?(config_file)
+  config = YAML.load_file(config_file)
+  config.merge!(config.delete(Rails.env)) if config.has_key?(Rails.env)
+  config.each do |k,v|
+    Rails.application.config.send("#{k}=", v)
+  end
+# end
+
+if smtp = Rails.application.config.smtp_settings
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = smtp
+  ActionMailer::Base.default_options = {from: Rails.application.config.email_from}
+end
